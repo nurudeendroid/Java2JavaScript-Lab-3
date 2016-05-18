38f957de7ac5ac2614cd4fe5831517eb497a768b
Froilan Miranda <froilan.miranda@gmail.com>
2016-05-18 09:38:02 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+" use strict ";
+
+function PhoneBook(){
+
+};
+
+function listAllNames() {
+  // code to list all names
+};
+
+function listAllNumbers() {
+  // code to list all numbers
+};
+
+function showAdd() {
+  var name = prompt("Enter full name");
+  var number = prompt("Enter phone number");
+};
+
+function showRemove() {
+  var remove = prompt("Enter name to remove");
+};
+
+function showLookup() {
+  var lookup = prompt("Enter number to lookup");
+};
+
+var display = document.getElementById("display");
